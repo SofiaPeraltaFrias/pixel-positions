fd000f2d2d58c55819f756398585deff3c3becf9
Sofia Peralta <sofiaperaltafrias@gmail.com>
2025-01-06 12:30:50 -0300

    Add job-card, tag, and section heading components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,16 @@
+<div class="p-4 bg-white/10 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Employer</div>
+    <div class="py-8 font-bold">
+        <h3>Job Title</h3>
+        <p>Schedule - Salary</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="http://place-hold.it/42/42" alt="" class="rounded-xl">
+    </div>
+</div>
